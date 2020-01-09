82a15bc837da74f682a469c8a0aa9b757b99a5bd
shivamb61 <shivam.bansal@comprotechnologies.com>
2020-01-09 13:21:46 +0530

    Smoke Test

--- nemoReset/pages/nemoSupportAdminDashboard.page.js
+++ nemoReset/pages/nemoSupportAdminDashboard.page.js
@@ -21,7 +21,7 @@ module.exports = {
             selector:'input[value="Verify"]'
         },
         search:{
-            selector:'[qid="sad-2"]'
+            selector:'[qid="sad-1"]'
         },
         searchbtn:{
             selector:'[qid="sad-3"]'
